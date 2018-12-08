785940522ce8a8f78e7122df86fcefee63eb2b40
Joel Ray Holveck <joelh@piquan.org>
2018-12-08 00:50:45 -0800

    Initial revision

---
+++ software/christmas-tree/flash.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+set -ex
+
+arduino=arduino
+avrdude=avrdude
+
+[ build/christmas-tree.ino.elf -nt christmas-tree.ino ] ||      \
+    "$arduino" --verify christmas-tree.ino                      \
+    --board attiny:avr:ATtinyX4:cpu=attiny84,clock=internal8    \
+    --preserve-temp-files --pref build.path=build/
+
+$avrdude -p attiny84 -c usbtiny                                 \
+    -U lfuse:w:0xe2:m -U hfuse:w:0xdd:m -U efuse:w:0xff:m       \
+    -U flash:w:build/christmas-tree.ino.hex:i
